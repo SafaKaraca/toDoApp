783bb531eabff50c8129ecff2e423abd28b3c18f
SafaKaraca <karacamuhammedsafa@gmail.com>
2023-12-07 21:51:47 +0300

    The 'textInput' functionality has been added.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,74 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from "react-native";
+import { useState } from "react";
 
 export default function App() {
 
+  const [enteredToDoText, setEnteredToDoText] = useState('')
+  const [courseToDos, setCourseToDos] = useState([])
+
+
+  function toDoInputHandler(enteredText) {
+    setEnteredToDoText(enteredText)
+
+  }
+
+  function addToDoHandler() {
+    setCourseToDos((currentCourseToDos) => [...currentCourseToDos, enteredToDoText,])
+  }
+
   return (
     <View style={Styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your ToDo's"/>
-        <Button title="Add a ToDo"></Button>
+      <View style={Styles.inputContainer}>
+        <TextInput style={Styles.textInput} placeholder="Your ToDo's" onChangeText={toDoInputHandler} />
+        <Button title="Add a ToDo" onPress={addToDoHandler}></Button>
       </View>
-      <View>
-        <Text>List of todo's</Text>
+      <View style={Styles.toDosContainer}>
+        <ScrollView alwaysBounceVertical={false}>
+          {courseToDos.map((todo) => (
+            <View key={todo} style={Styles.todoItem}>
+              <Text style={Styles.todoText} >{todo}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   )
 }
 
 const Styles = StyleSheet.create({
-  appContainer:{
-    padding:50,
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    margin: 8,
+    padding: 8,
+  },
+  toDosContainer: {
+    flex: 5
+  },
+  todoItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  todoText: {
+    color: 'white'
+  }
+
 
 });
